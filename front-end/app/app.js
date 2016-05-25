d7f5e2099e59cf67fab0de615b13f55eb769c9af
NicolaRettich1 <nicola.rettich@posteo.de>
2016-05-25 12:10:58 +0200

    config app.js Test

--- front-end/app/app.js
+++ front-end/app/app.js
@@ -1,3 +1,8 @@
 /**
  * Created by nicol on 25.05.2016.
  */
+var  app = angularModule("GoOutSmart", [])
+
+app.controller("HomeController", function($scope){
+    $scope.message = "Hallo Angular";
+});
