6eda87673b8aa6c14ecbd0a341a1289d1be8e920
NicolaRettich1 <nicola.rettich@posteo.de>
2016-05-25 13:21:22 +0200

    Ordnerstruktur angepasst

--- front-end/app/app.js
+++ front-end/app/app.js
@@ -3,6 +3,11 @@
  */
 var  app = angularModule("GoOutSmart", [])
 
-app.controller("HomeController", function($scope){
+app.controller("HomeController", function($scope, $http){
     $scope.message = "Hallo Angular";
+//    $http.get("http://localhost:63342/Contact").success(function (response) {
+//        $scope.contacts = response;
+//    }).error(function (err) {
+//        $scope.error = err;
+//    });
 });
