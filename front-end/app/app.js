95bc59c68b13989a0f5d0b5ebe881192a5f86d8a
Maximilian Frei <ga68zal@mytum.de>
2016-05-25 16:44:16 +0200

    On the way to fix the angular message

--- front-end/app/app.js
+++ front-end/app/app.js
@@ -1,9 +1,9 @@
 /**
  * Created by nicol on 25.05.2016.
  */
-var  app = angularModule("GoOutSmart", [])
+var  app = angularModule("app", []);
 
-app.controller("HomeController", function($scope, $http){
+app.controller("HomeController", function($scope){
     $scope.message = "Hallo Angular";
 //    $http.get("http://localhost:63342/Contact").success(function (response) {
 //        $scope.contacts = response;
